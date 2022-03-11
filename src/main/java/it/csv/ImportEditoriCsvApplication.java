c8efd2cd275482b6794d7fe30276c3c40cec69a0
Simone Guidi <gsmakeup92@gmail.com>
2022-03-11 15:57:15 +0100

    Initial commit

---
+++ src/main/java/it/csv/ImportEditoriCsvApplication.java
@@ -0,0 +1,13 @@
+package it.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportEditoriCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportEditoriCsvApplication.class, args);
+	}
+
+}
